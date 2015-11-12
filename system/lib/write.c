5c47343b4da131d4247b3505652c90562d88d8d2
cdai <dc_726@163.com>
2015-11-12 13:23:35 +0800

    add fork syscall

--- system/lib/write.c
+++ system/lib/write.c
@@ -3,8 +3,8 @@
 int write(u32 fd, const char *buf, u32 cnt)
 {
 	long res;
-        __asm__(
-                "int 0x80"
+        __asm__ volatile (
+                "int $0x80"
                 : "=a"(res)
                 : "0"(4), "b"(fd), "c"((u32)buf), "d"(cnt)
         );
